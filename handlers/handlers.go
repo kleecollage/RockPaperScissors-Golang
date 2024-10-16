1bc7b62274b898b4fbe9b607b3fde8cd41cccdee
Klee Collage <klee.collage@gmail.com>
2024-10-16 14:18:15 -0500

    Update handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,26 +1,43 @@
 package handlers
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"text/template"
+)
+
+const (
+	templateDir  = "templates/"
+	templateBase = templateDir + "base.html"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Index is working!")
+	renderTemplate(w, "index.html", nil)
 }
 
 func NewGame(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Crear nuevo juego")
+	renderTemplate(w, "new-game.html", nil)
 }
 
 func Game(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Game is working!")
+	renderTemplate(w, "game.html", nil)
 }
 
 func Play(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Play is working!")
+	// TODO template
+	renderTemplate(w, "index.html", nil)
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "About is working!")
+	renderTemplate(w, "about.html", nil)
+}
+
+func renderTemplate(w http.ResponseWriter, page string, data any) {
+	tmpl := template.Must(template.ParseFiles(templateBase, templateDir+page))
+	err := tmpl.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		http.Error(w, "Error al renderizar plantillas", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
 }
